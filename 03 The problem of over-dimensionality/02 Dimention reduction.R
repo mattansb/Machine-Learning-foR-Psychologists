18912966bd32810991feb02ccec058ccc5728b61
Mattan S. Ben-Shachar <mattansb@msbstats.info>
2023-08-07 21:42:46 +0300

    switch order and clean up PCA

--- 03 The problem of over-dimensionality/02 Dimention reduction.R
+++ 03 The problem of over-dimensionality/02 Dimention reduction.R
@@ -15,13 +15,8 @@ splits <- initial_split(Hitters, prop = 0.7)
 Hitters_train <- training(splits)
 Hitters_test <- testing(splits)
 
-# PCR --------------------------------------------------------------------
-# This is a method that use PCA as a first step for predicting y. 
-# IMPORTANT- this is an UNSUPERVISED method for dimension reduction - since PCA
-# don't uses a response variable when building the components. There are other
-# methods, which uses the response (hence, SUPERVISED), for using a similar
-# goal- predicting y from components
 
+# PCR and PLS -------------------------------------------------------------
 
 ## (A) Prepossessing
 
@@ -32,6 +27,12 @@ rec <- recipe(Salary ~ ., data = Hitters_train) |>
   # Make one hot encoding dummy variables
   step_dummy(all_nominal_predictors(), one_hot = TRUE)
 
+## PCR -----------------------------------
+# This is a method that use PCA as a first step for predicting y. 
+# IMPORTANT- this is an UNSUPERVISED method for dimension reduction - since PCA
+# don't uses a response variable when building the components. There are other
+# methods, which uses the response (hence, SUPERVISED), for using a similar
+# goal- predicting y from components
 
 
 ## (B) Tune:
@@ -57,11 +58,12 @@ summary(PCR_fit$finalModel)
 coef(PCR_fit$finalModel, ncomp = 7)
 
 
-# PLS ----------------------------------------
+## PLS ---------------------------
 # We will now address the same problem using another dimension reduction method-
 # PLS. Importantly, PCR is an UNSUPERVISED method and PLS is a SUPERVISED
 # method. PLS counts for *Partial Least Squares*.
 
+## (B) Tune:
 
 PLS_fit <- train(
   x = rec,
@@ -78,7 +80,7 @@ summary(PLS_fit$finalModel)
 coef(PLS_fit$finalModel, ncomp = 3)
 
 
-# Compare ----------------
+## Compare ----------------
 
 cbind(coef(PCR_fit$finalModel, ncomp = 2),
       coef(PLS_fit$finalModel, ncomp = 2))
@@ -93,3 +95,56 @@ rmse(Hitters_test, Salary, PLS_pred)
 rsq(Hitters_test, Salary, PCR_pred)
 rsq(Hitters_test, Salary, PLS_pred)
 # In this case the performance is nearly identical
+
+
+# Using PCA in other methods w/ {recipe} ----------------------------------
+
+# What if I want to use KNN??
+# We can still use PCA as part of our recipe (however, the number of dimensions
+# is no longer a tunable hyperparameter). This can be achieved with
+# `step_pca()`. There are two arguments that can be used to control how many PCs
+# to save:
+# - num_comp: the number of components
+# - threshold: what proportion of variance should be saved?
+# * Note that the predictors should be all be re-scaled _prior_ to the PCA step.
+
+
+rec
+# Already has a scaling step and centering step
+
+rec_with_PCA <- rec |> 
+  step_pca(all_numeric_predictors(), 
+           threshold = 0.9) # give k PCs that represent 90% of the total variance
+
+## (B) Tune:
+
+tg <- expand.grid(k = c(1, 2, 5, 10, 20, 50, 100))
+
+set.seed(44)
+KNN_fit <- train(
+  x = rec_with_PCA, 
+  data = Hitters_train,
+  method = "knn",
+  tuneGrid = tg,
+  trControl = tc
+)
+
+PLS_fit$recipe |> bake(new_data = NULL) # We had 22 predictors
+KNN_fit$recipe |> bake(new_data = NULL) # We used 7 PCs
+
+## Compare -------------------
+
+Hitters_test$KNN_pred <- predict(KNN_fit, newdata = Hitters_test)
+
+rsq(Hitters_test, Salary, PCR_pred)
+rsq(Hitters_test, Salary, PLS_pred)
+rsq(Hitters_test, Salary, KNN_pred)
+# KNN with PCA is better...
+
+
+
+# How about KNN without PCA?
+
+
+
+
