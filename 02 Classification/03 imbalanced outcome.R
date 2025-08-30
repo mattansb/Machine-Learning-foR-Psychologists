b254a1ebe6019476ee51f3e498eb68fc5e635851
Mattan S. Ben-Shachar <mattansb@msbstats.info>
2025-08-30 22:26:40 +0300

    use nullmodel

--- 02 Classification/03 imbalanced outcome.R
+++ 02 Classification/03 imbalanced outcome.R
@@ -30,29 +30,48 @@ table(Caravan.train$Purchase) |> proportions()
 table(Caravan.test$Purchase) |> proportions()
 
 
+# We will be using this recipe:
+rec <- recipe(Purchase ~ ., 
+              data = Caravan.train) |> 
+  step_normalize(all_numeric_predictors())
+
+# And these classification metrics:
+mset_class <- metric_set(accuracy, specificity, sensitivity)
+
 
 # The worst model ---------------------------------------------------------
 # The data imbalance means that, technically, we can achieve high accuracy by
 # simply predicting "No"....
 
-Caravan.test$.pred_class.BAD <- factor("No", levels = c("Yes", "No"))
-Caravan.test$.pred_Yes.BAD <- 0
+null_fit <-
+  workflow(preprocessor = rec,
+           spec = null_model(mode = "classification")) |>
+  fit(data = Caravan.train)
+# The null model is the worst possible model - it does not use any information
+# in X, only the distribution of Y in the training set.
+# - For regression, it always predicts mean(Y)
+# - For classification, it predicts the frequent class and base rate 
+#   probabilities.
+# These models are good for benchmarking.
 
-Caravan.test |> conf_mat(truth = Purchase, estimate = .pred_class.BAD)
+# But like a "real" model, it can be used to generate predictions on new data:
+Caravan.test_predictions_NULL <- augment(null_fit, new_data = Caravan.test)
 
+# Same as:
+# Caravan.test$.pred_class.BAD <- factor("No", levels = c("Yes", "No"))
+# Caravan.test$.pred_Yes.BAD <- 0
+
+
+Caravan.test_predictions_NULL |>
+  conf_mat(truth = Purchase, estimate = .pred_class)
 
 # But as we can see, we have no specificity!
-mset_class <- metric_set(accuracy, specificity, sensitivity)
-Caravan.test |> mset_class(truth = Purchase, estimate = .pred_class.BAD)
+Caravan.test_predictions_NULL |>
+  mset_class(truth = Purchase, estimate = .pred_class)
 
 
 
 # Training with Imbalance Data --------------------------------------------
-
-rec <- recipe(Purchase ~ ., 
-              data = Caravan.train) |> 
-  step_normalize(all_numeric_predictors())
-
 
 knn_spec <- nearest_neighbor(
   mode = "classification", engine = "kknn", 
@@ -92,37 +111,41 @@ knn_fit.down <- knn_wf |>
   update_recipe(rec_down) |> 
   fit(data = Caravan.train)
 
+
 # Comparing Results -------------------------------------------------------
 
 # Get raw predictions
-Caravan.test_predictions <- augment(knn_fit, new_data = Caravan.test)
-Caravan.test_up.predictions <- augment(knn_fit.up, new_data = Caravan.test)
-Caravan.test_down.predictions <- augment(knn_fit.down, new_data = Caravan.test)
+Caravan.test_predictions <- 
+  bind_rows(
+    "NULL" = Caravan.test_predictions_NULL,
+    None = augment(knn_fit, new_data = Caravan.test),
+    Up = augment(knn_fit.up, new_data = Caravan.test),
+    Down = augment(knn_fit.down, new_data = Caravan.test), 
+    
+    .id = "Method"
+  ) |> 
+  mutate(
+    Method = factor(Method, levels = c("NULL", "None", "Up", "Down"))
+  )
 
-
-Caravan.test |> 
-  mset_class(truth = Purchase, estimate = .pred_class.BAD)
+# Let's look at the predictions made by the different methods for the first test
+# case:
 Caravan.test_predictions |> 
-  mset_class(truth = Purchase, estimate = .pred_class)
-Caravan.test_up.predictions |> 
-  mset_class(truth = Purchase, estimate = .pred_class)
-Caravan.test_down.predictions |> 
-  mset_class(truth = Purchase, estimate = .pred_class)
+  select(Method, Purchase, starts_with(".pred")) |>
+  slice(1, .by = Method)
 
+
+
+Caravan.test_predictions |> 
+  group_by(Method) |> 
+  mset_class(truth = Purchase, estimate = .pred_class)
 # As we can see, the accuracy (and specificity) have dropped, but sensitivity is
 # higher.
 
 
 
 # We can also compare ROC curves and AUCs:
-ROCs <- bind_rows(
-  Fixed = Caravan.test |> roc_curve(truth = Purchase, .pred_Yes.BAD),  
-  None = Caravan.test_predictions |> roc_curve(truth = Purchase, .pred_Yes),
-  Up = Caravan.test_up.predictions |> roc_curve(truth = Purchase, .pred_Yes),
-  Down = Caravan.test_down.predictions |> roc_curve(truth = Purchase, .pred_Yes), 
-  
-  .id = "Method"
-)
-
-autoplot(ROCs) + aes(color = Method)
-
+Caravan.test_predictions |> 
+  group_by(Method) |> 
+  roc_curve(truth = Purchase, .pred_Yes) |> 
+  autoplot()
