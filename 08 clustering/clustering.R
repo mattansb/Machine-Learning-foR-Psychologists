ab58fd5c8ff2e2ec965b813e89d28c7fbcb4de44
Mattan S. Ben-Shachar <mattansb@msbstats.info>
2026-05-30 16:43:56 +0300

    init

--- 08 clustering/clustering.R
+++ 08 clustering/clustering.R
@@ -1,12 +1,15 @@
 ### Tutorial 6- Clustering  ###
 
 library(tidymodels)
+library(tidyclust)
+library(cluster)
+library(factoextra)
+library(philentropy)
 
 library(patchwork)
 
-library(cluster)
 library(Rtsne)
-library(factoextra) # https://rpkgs.datanovia.com/factoextra/index.html
+
 
 # Learn more at:
 # https://tidyclust.tidymodels.org/index.html
@@ -15,6 +18,8 @@ library(factoextra) # https://rpkgs.datanovia.com/factoextra/index.html
 
 data(USArrests)
 ?USArrests # Violent Crime Rates by US State
+USArrests <- USArrests |> tibble::rownames_to_column("state.name")
+head(USArrests)
 
 # Are there clusters of states that are similar in their rates of crime?
 
@@ -26,15 +31,12 @@ summary(USArrests)
 # We should re-scale them.
 
 # We can use recipe:
-rec <- recipe(~., data = USArrests) |>
+rec <- recipe(~ Murder + Assault + UrbanPop + Rape, data = USArrests) |>
   step_normalize(all_numeric_predictors())
 
-USArrests_z <- bake(prep(rec), new_data = USArrests, composition = "data.frame")
-rownames(USArrests_z) <- rownames(USArrests) # we need to re-add rownames :/
+USArrests_z <- prep(rec) |> bake(new_data = USArrests)
 
-head(USArrests_z)
-
-plot(USArrests_z, pch = 20, cex = 2)
+plot(USArrests[, -1], pch = 20, cex = 2)
 # there are some associations between variables, but are there CLUSTERS?
 
 ## t-SNE plot -------------------------------------------------
@@ -51,13 +53,14 @@ plot(USArrests_z, pch = 20, cex = 2)
 # so don't forget to set a seed:
 set.seed(20251109)
 
-USArrests_z_tSNE <- Rtsne(USArrests_z, perplexity = 5)
+
+USArrests_tSNE <- Rtsne(USArrests_z, perplexity = 5)
 # Default perplexity is 30, but this value is too large for our small dataset.
 
-p_tSNE <- data.frame(USArrests_z_tSNE$Y) |>
+p_tSNE <- data.frame(USArrests_tSNE$Y) |>
   ggplot(aes(X1, X2)) +
   geom_point(size = 2) +
-  ggrepel::geom_text_repel(aes(label = rownames(USArrests_z))) +
+  ggrepel::geom_text_repel(aes(label = USArrests$state.name)) +
   # scales are meaningless, so remove them
   theme_void()
 p_tSNE
@@ -74,88 +77,85 @@ p_tSNE
 # This is a "Top Down" approach - cluster observations on the basis of the
 # features. Prior selection of K - number of clusters.
 
-## Choosing number of clusters ---------------------
+# We will be using k-means clustering, which is a popular partitioning
+# clustering method.
+
+km_spec <- k_means(
+  mode = "partition",
+  engine = "stats",
+  num_clusters = tune() # we will... tune this?
+) |>
+  set_args(nstart = 20) # how many random starting centers
+# NOTE: If nstart > 1, then K-means clustering will be performed using multiple
+# random assignments in Step 1 of the Algorithm, and kmeans() will report only
+# the best results.
+# It is recommended always running K-means clustering with a large start, such
+# as 20 or 50, since otherwise an undesirable local optimum may be obtained.
+
+km_wf <- workflow(preprocessor = rec, spec = km_spec)
+
+## Tuning the number of clusters -----------------
 # https://www.datanovia.com/en/lessons/determining-the-optimal-number-of-clusters-3-must-know-methods/
 
+# Unlike supervised learning, there is no "ground truth" to compare our clusters
+# to. Instead, we can use methods that evaluate the quality of the clustering
+# based on the data itself.
+
+km_tuner <- tune_cluster(
+  km_wf,
+  resamples = vfold_cv(USArrests, v = 5),
+  grid = tibble(num_clusters = 1:10),
+  metrics = cluster_metric_set(silhouette_avg, sse_within_total)
+)
+
 ## Elbow method: drop in within-cluster variance:
-fviz_nbclust(
-  USArrests_z,
-  FUNcluster = kmeans, # we will use kmeans for clustering
-  method = "wss",
-  k.max = 20
-) +
-  geom_vline(xintercept = 4, linetype = 2) +
-  labs(subtitle = "Elbow method")
+autoplot(km_tuner, metric = "sse_within_total")
 # Note that, the elbow method is sometimes ambiguous (like here).
 
 ## Average silhouette method: Measures the quality of a clustering - how well
 # each object lies within its cluster. A high average silhouette width indicates
 # a good clustering.
-fviz_nbclust(
-  USArrests_z,
-  FUNcluster = kmeans,
-  method = "silhouette",
-  k.max = 20
-) +
-  labs(subtitle = "Silhouette method")
+autoplot(km_tuner, metric = "silhouette_avg")
 
-
-## Gap statistic (using bootstrap sampling): measures how far is the observed
-# within intra-cluster variation is from a random uniform distribution of the
-# data?
-fviz_nbclust(
-  USArrests_z,
-  FUNcluster = kmeans,
-  method = "gap_stat",
-  k.max = 20
-) +
-  labs(subtitle = "Gap statistic method")
-# Suggests k=2 or k=4
 
 ## Finding Clusters with k-means ------------------------------
 
-# The function kmeans() performs K-means clustering.
-km <- kmeans(
-  USArrests_z, # Features to guide clustering
-  centers = 4, # K
-  nstart = 20 # how many random starting centers
+km_fit <- km_wf |>
+  finalize_workflow(list(num_clusters = 4)) |>
+  fit(data = USArrests)
+
+
+extract_centroids(km_fit) # the clusters' centers (on the preprocessed data)
+# We can now label the clusters based on these centers.
+# ... and extract a the assignment of observations to each cluster:
+km_clusters <- extract_cluster_assignment(
+  km_fit,
+  labels = c("High Rural", "High Urban", "Low Urban", "Low Rural")
 )
-# NOTE: If nstart > 1, then K-means clustering will be performed using multiple
-# random assignments in Step 1 of the Algorithm, and kmeans() will report only
-# the best results.
-# It is recommended always running K-means clustering with a large start, such
-# as 20 or 50, since otherwise an undesirable local optimum may be obtained
 
-km$cluster # vector which assigns obs. to each cluster
-km$size # obs. num in each cluster
+# obs. num in each cluster
+table(km_clusters$.cluster)
 
-km$centers # the two clusters' centers
+# Add the cluster labels to the original data:
+USArrests$km_cluster <- km_clusters$.cluster
+
 # We can also find the centers on the original scale:
 USArrests |>
-  group_by(km = km$cluster) |>
-  summarise(across(everything(), mean))
-# Can now label the clusters based on these centers:
-cluster_km <- factor(
-  km$cluster,
-  labels = c("High Rural", "Low Rural", "High Urban", "Low Urban")
-)
+  group_by(km_cluster) |>
+  summarise(across(Murder:Rape, mean))
 
 
 # Plotting each observation colored according to its cluster assignment:
-plot(USArrests, col = cluster_km, pch = 20, cex = 2)
+plot(USArrests[, 2:5], col = USArrests$km_cluster, pch = 20, cex = 2)
 
 # How does this look on our t-SNE plot?
-p_tSNE + aes(color = cluster_km)
-
-
-# Let's save these results for later:
-dta_clust <- tibble(state.name, state.abb, kmeans = cluster_km)
+p_tSNE + aes(color = USArrests$km_cluster)
 
 
 ## More partitioning methods ---------------------------
 
-# The {cluster} package provides several other clustering algorithms, such as
-# PAM and CLARA. See:
+# The {cluster} package provides several other clustering algorithms (which
+# don't work with {tidyclust} just yet), such as PAM and CLARA. See:
 # https://cran.r-project.org/web/packages/cluster/refman/cluster.html
 
 # Hierarchical Clustering ---------------------------------------------------------
@@ -169,85 +169,116 @@ dta_clust <- tibble(state.name, state.abb, kmeans = cluster_km)
 
 ## Distance metric -----------------------------------
 # There are several to choose from....
-?get_dist
+?philentropy::distance()
 
-USArrests_d_euc <- get_dist(USArrests_z, method = "euclidean")
-USArrests_d_cor <- get_dist(USArrests_z, method = "pearson")
+distance(
+  USArrests_z,
+  method = "euclidean"
+)
+
+distance(
+  USArrests_z,
+  method = "cosine"
+)
 
 # We can also use a type of unsupervised random forest to get a distance matrix:
 # https://gradientdescending.com/unsupervised-random-forest-example/
-rf <- randomForest::randomForest(
-  x = USArrests,
-  mtry = sqrt(ncol(USArrests)),
-  ntree = 1000,
-  proximity = TRUE
-)
-USArrests_d_rf <- as.dist(1 - rf$proximity)
+dist_rf <- function(x) {
+  rf <- randomForest::randomForest(
+    x = x,
+    mtry = sqrt(ncol(x)),
+    ntree = 1000,
+    proximity = TRUE
+  )
+  return(1 - rf$proximity)
+}
 
-# Which should we use?
-fviz_dist(USArrests_d_euc)
-fviz_dist(USArrests_d_cor)
-fviz_dist(USArrests_d_rf)
 
+# We will use the Euclidean distance (which is the default), but you should feel
+# free to experiment with other distance metrics and see how they affect your
+# results.
 
 ## Build dendrogram -----------------
 # The hclust() function implements hierarchical clustering.
 
-hc.complete <- hclust(USArrests_d_euc, method = "complete")
-# Or method = "average"
-# Or method = "single"
-# Or method = "centroid"
+hc_spec <- hier_clust(
+  mode = "partition",
+  engine = "stats",
+
+  # we can actually select this later!
+  num_clusters = NULL,
+  cut_height = NULL,
+
+  # Decisions!
+  linkage_method = "complete",
+  dist_fun = \(x) distance(x, method = "euclidean") # (this is the default)
+)
+# Or linkage_method = "average"
+# Or linkage_method = "single"
+# Or linkage_method = "centroid"
+
+hc_wf <- workflow(preprocessor = rec, spec = hc_spec)
+
+hc_fit <- hc_wf |> fit(data = USArrests)
+
 
 # plotting the dendrograms (with small-ish samples) and determining clusters:
-plot(hc.complete)
+hc_eng <- extract_fit_engine(hc_fit)
+plot(hc_eng)
+
 # The numbers / labels at the bottom of the plot identify each observation.
 
 # We can also use:
-fviz_dend(hc.complete)
-fviz_dend(hc.complete, k = 4)
-fviz_dend(hc.complete, h = 3) +
-  geom_hline(yintercept = 3, linetype = 2)
+fviz_dend(hc_eng)
+fviz_dend(hc_eng, k = 4)
+fviz_dend(hc_eng, h = 3) + geom_hline(yintercept = 3, linetype = 2)
+
+# We can also extract the cluster centers (on the preprocessed data):
+extract_centroids(hc_fit, num_clusters = 4) # Looks similar to k-means centers)
 
 # See also:
-# bannerplot(hc.complete)
+# ?cluster::bannerplot(hc_eng)
 
 ## Cut the tree! -----------------------------------
 
 # To determine the cluster labels for each observation associated with a given
 # cut of the dendrogram, we can use the cutree() function:
-(hc_cut.k4 <- cutree(hc.complete, k = 4))
-(hc_cut.h1 <- cutree(hc.complete, h = 1))
 
+hc_clusters.k4 <- hc_fit |>
+  extract_cluster_assignment(
+    num_clusters = 4,
+    labels = c("High Rural", "High Urban", "Low Urban", "Low Rural")
+  )
+
+
+# Add the cluster labels to the original data:
+USArrests$hc_cluster.k4 <- hc_clusters.k4$.cluster
 
 # Plotting each observation colored according to its cluster assignment:
-plot(USArrests, col = hc_cut.k4, pch = 20, cex = 2)
+plot(USArrests[, 2:5], col = USArrests$hc_cluster.k4, pch = 20, cex = 2)
 
 # How does this look on our t-SNE plot?
-p_tSNE + aes(color = factor(hc_cut.k4))
-
-# Save the results:
-dta_clust$hclust_k4 <- factor(hc_cut.k4[state.name])
-
-
-# Do the methods agree?
-USArrests_z |>
-  group_by(km = hc_cut.k4) |>
-  summarise(across(everything(), mean))
-# This looks very similar to k-means results...
+p_tSNE + aes(color = factor(USArrests$hc_cluster.k4))
 
 table(
-  "H-Clust" = dta_clust$hclust_k4,
-  "K-mean" = dta_clust$kmeans
+  "H-Clust" = USArrests$hc_cluster.k4,
+  "K-mean" = USArrests$km_cluster
 )
 
+# See also:
+?reconcile_clusterings_mapping
+
 # Model based clustering -----------------------------------
-#
-# See the {mclust} package
 # https://mclust-org.github.io/mclust/
+?gm_clust
+
 
 # Understanding Clusters ------------------------------------------------------
 # All clustering methods will produce clusters, even if there is no real
 # structure in the data. So, it is important to validate the clusters.
+#
+# Unfortunately, {tidyclust} doesn't have built-in functions for cluster
+# validation, but we can use other packages for this.
 
 ## Internal Validation? -------------------------------------------------
 # How "good" is the clustering?
@@ -261,13 +292,15 @@ table(
 # clusters, and a value close to -1 indicates that the observation may have been
 # assigned to the wrong cluster.
 
-sil_km <- silhouette(
-  km$cluster,
-  dist = USArrests_d_euc # k-means uses euclidean distance
+sil_km <- cluster::silhouette(
+  x = as.integer(USArrests$km_cluster),
+  dist = distance(USArrests_z, method = "euclidean") # k-means uses euclidean distance
 )
+
 plot(sil_km, col = sample(colors(TRUE), 4))
 summary(sil_km)
-# Overall the values are adequate, but not amazing...
+# Overall the values are adequate (sil>~0.3), but not amazing (ideally we would
+# want sil>0.5)...
 
 ### Cluster stability ----------------------------
 # Are the clusters stable to small perturbations in the data? In other words,
@@ -284,7 +317,8 @@ jac_kmeans <- clusterboot(
 )
 
 jac_hclust <- clusterboot(
-  USArrests_d_euc,
+  distance(USArrests_z, method = "euclidean"),
+  distances = TRUE,
   B = 200,
 
   clustermethod = hclustCBI,
@@ -296,16 +330,17 @@ jac_hclust <- clusterboot(
 # 0.5 indicate highly unstable clusters.
 
 jac_kmeans # all clusters are stable
-jac_hclust # cluster 2 and (4?) are unstable
+jac_hclust # cluster 2 might be unstable?
 
 
 ### Compare cluster means ---------------
 # Do the clusters differ on the variables that went into the clustering?
 
 # Let's use k-means clusters for this example. Recall:
-km$centers
-# Do clusters 1 and 3 differ on these variables?
-# We cant just compute t-tests, because that would be doing post-selection
+extract_centroids(km_fit)
+
+# Do clusters 1 and 2 actually differ on these variables?
+# We can't just compute t-tests, because that would be doing post-selection
 # inference!
 # Thankfully, the {clusterpval} package provides functions to test for
 # differences between clusters while accounting for the fact that the clusters
@@ -315,16 +350,30 @@ km$centers
 library(clusterpval)
 
 # Make a function to obtain clusters:
-cl_fun = \(X) kmeans(X, centers = 4, nstart = 20)$cluster
+cl_fun = function(X) {
+  # K-means clustering with 4 clusters:
+  km_spec |>
+    finalize_model(list(num_clusters = 4)) |>
+    # "fit" the model to the data:
+    fit_xy(X) |>
+    # Extract the cluster assignment:
+    extract_cluster_assignment() |>
+    # pull the cluster labels and convert to integer:
+    pull(.cluster) |>
+    as.integer()
+}
+
 
 test_clusters_approx(
   X = as.matrix(USArrests_z),
   # Clusters to compare
   k1 = 1,
-  k2 = 3,
+  k2 = 2,
 
   cl_fun = cl_fun,
-  cl = km$cluster
+  cl = as.integer(USArrests$km_cluster),
+
+  ndraws = 500
 )
 
 # How about clusters 1 and 4?
@@ -335,7 +384,9 @@ test_clusters_approx(
   k2 = 4,
 
   cl_fun = cl_fun,
-  cl = km$cluster
+  cl = as.integer(USArrests$km_cluster),
+
+  ndraws = 500
 )
 
 
@@ -346,26 +397,17 @@ test_clusters_approx(
 # clusters be used to tell us something new about other variables?
 
 # Let's compare them to some data about the states:
-states_info <- read.csv("states_info.csv") |>
-  left_join(dta_clust, by = c("state.abb", "state.name"))
+states_info <- read.csv("states_vote1981.csv") |>
+  left_join(USArrests, by = join_by(state == state.name))
 head(states_info)
 
 
-# Are the clusters related to voting Trump in 2024?
-p_trump <-
-  ggplot(states_info, aes(trump2024, kmeans, fill = kmeans)) +
-  geom_vline(xintercept = 0.5) +
+# Are the clusters related to voting for Reagan in 1981?
+ggplot(states_info, aes(margin_pct, km_cluster, fill = km_cluster)) +
+  geom_vline(xintercept = 0) +
   geom_violin() +
-  scale_x_continuous("% voted for Trump", limits = c(0, 1))
-
-
-# Are the clusters related to number of casinos?
-p_casino <-
-  ggplot(states_info, aes(n_casinos, kmeans, fill = kmeans)) +
-  geom_violin()
-
-p_trump + p_casino + plot_layout(guides = "collect")
-
+  scale_x_continuous("% margin voted for Reagan")
+# What can we learn from this?
 
 # Exercise ----------------------------------------------------------------
 
